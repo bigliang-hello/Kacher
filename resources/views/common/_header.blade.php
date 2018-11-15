fe7d37fc7246a6a5f2df04ea09fe064cf4d57722
bigliang_iOS <xlhelloworld@163.com>
2018-11-15 10:29:22 +0800

    header and footer

---
+++ resources/views/common/_header.blade.php
@@ -0,0 +1,38 @@
+<nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+    <div class="container">
+        <!-- Brand/logo -->
+        <a class="navbar-brand mr-0 mr-md-2" href="#">Kacher</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse ml-2" id="collapsibleNavbar">
+            <ul class="navbar-nav">
+                <li class="mx-2">
+                    <a class="nav-link active" href="#">全部</a>
+                </li>
+                <li class="mx-2">
+                    <a class="nav-link" href="#">交流区</a>
+                </li>
+                <li class="mx-2">
+                    <a class="nav-link" href="#">交易区</a>
+                </li>
+            </ul>
+
+            <ul class="navbar-nav ml-auto">
+                <li class="mx-2">
+                    <a class="nav-link" href="#">登录</a>
+                </li>
+                <li class="mx-2">
+                    <a class="nav-link" href="#">注册</a>
+                </li>
+            </ul>
+        </div>
+
+    </div>
+
+</nav>
+
+<header class="bs-docs-nav navbar navbar-static-top" id="top"></header>
